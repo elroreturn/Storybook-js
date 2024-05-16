a1d52eca87301b3e3ee206283fa9e302bc23f5bb
Rodrigo Antognazza <elroreturn@users.noreply.github.com>
2024-05-16 20:17:22 -0300

    Base CalcApp

---
+++ calc-app/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    console.log("Renderización de App")
+    return (
+    <main className='react-calculator'>
+        <div className="result">
+        </div>
+        <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+        </div>
+        <div className="functions">
+            <button>
+                Clear
+            </button>
+            <button>
+                Reset
+            </button>
+        </div>
+        <div className="math-operations">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+            <button>=</button>
+        </div>
+    </main>)
+}
+
+export default App
+
